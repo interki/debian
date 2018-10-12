fa7ced7adedd39dc0d00101267c01edfe3c9243f
interki <tom_is@hotmail.com>
2018-10-13 06:44:58 +1000

    up

--- windows scripts/alarms/alarm.set.enable.bat
+++ windows scripts/alarms/alarm.set.enable.bat
@@ -2,9 +2,11 @@ Schtasks /change /st 05:15 /tn "volume-up"
 Schtasks /change /st 05:16 /tn "un-mute"
 Schtasks /change /st 05:20 /tn "alarm-a"
 Schtasks /change /st 05:30 /tn "alarm-b"
+Schtasks /change /st 05:35 /tn "volume-down"
 
 Schtasks /change /tn "volume-up" /ENABLE
 Schtasks /change /tn "un-mute" /ENABLE
 Schtasks /change /tn "alarm-a" /ENABLE
 Schtasks /change /tn "alarm-b" /ENABLE
+Schtasks /change /tn "volume-down" /ENABLE
 PAUSE
